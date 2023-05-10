e682592fce1bba2fcfd43e78d4661716c6053f57
Prajakta K Kale <prajaktakkale@gmail.com>
2023-05-11 02:44:24 +0530

    first commit

---
+++ sample.js
@@ -0,0 +1,21 @@
+function loadCoupon(){
+    
+    document.getElementById('main').style.opacity = '1';
+    document.getElementById('coupon').style.visibility = 'visible';
+}
+
+function closeCoupon(){
+    document.getElementById('main').style.opacity = '0';
+    document.getElementById('coupon').style.visibility = 'hidden';
+}
+
+var icon= document.getElementById("icon")
+        icon.onclick = function(){
+            document.body.classList.toggle("dark-theme");
+            if(document.body.classList.contains("dark-theme")){
+                icon.src = "lighton.png";
+            }
+            else{
+                icon.src = "lightoff.png";
+            }
+        }
